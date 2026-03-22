ebcef11e581ea513ffaf43d3a53bf1086c6aede1
Tomas <tomaslonnblad@gmail.com>
2026-03-22 20:04:56 +0100

    Update sparkles-demo.tsx

--- components/sparkles-demo.tsx
+++ components/sparkles-demo.tsx
@@ -4,11 +4,11 @@ import { SparklesCore } from "@/components/ui/sparkles";
 
 export default function SparklesPreview() {
   return (
-    <div className="h-[400rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+    <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
         Tomas land of viking
       </h1>
-      <div className="w-[40rem] h-40 relative">
+      <div className="w-[4rem] h-4 relative">
         {/* Gradients */}
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
