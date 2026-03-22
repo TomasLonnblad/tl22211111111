393958d4a77c069e2f2f7d3628a5f98be1882075
Tomas <tomaslonnblad@gmail.com>
2026-03-22 19:24:54 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Component from '@/components/sparkles-demo'
 
 export default function Page() {
   return (
-    <main className="flex min-h-svh items full-screen">
+    <main className="items full-screen">
       <Component />
     </main>
   )
